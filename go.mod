0ff9aa6b2e46060a2764903b6522fe7ffcb1fd97
Vitaly Bolychev <cybervito21@gmail.com>
2019-05-16 16:28:31 +0300

    make generators separated from gen

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.12
 require (
 	github.com/dave/jennifer v1.3.0
 	github.com/fatih/structtag v1.0.0 // indirect
-	github.com/iancoleman/strcase v0.0.0-20180726023541-3605ed457bf7
+	github.com/hashicorp/go-plugin v1.0.0
 	github.com/pkg/errors v0.8.1
 	github.com/sergi/go-diff v1.0.0
 	github.com/shopspring/decimal v0.0.0-20180709203117-cd690d0c9e24
---
+++ pkg/plugin.go
@@ -0,0 +1,53 @@
+package pkg
+
+import (
+	"net/rpc"
+
+	plugin "github.com/hashicorp/go-plugin"
+)
+
+// It is default command for each generator plugin.
+const cmdPluginGenerate = "Plugin.Generate"
+
+// It is a plugin client.
+type Client struct {
+	client *rpc.Client
+}
+
+// Generate method implements Generator interface.
+// It calls plugin generation in special net/rpc format.
+func (g *Client) Generate(params *GenerateParams) (*GenerateResult, error) {
+	result := &GenerateResult{}
+	err := g.client.Call(cmdPluginGenerate, params, &result)
+	if err != nil {
+		panic(err)
+	}
+
+	return nil, nil
+}
+
+// Server allows to serve a request to plugin.
+type Server struct {
+	Impl Generator
+}
+
+// Generate runs underlying implementation of code generator.
+func (g *Server) Generate(args *GenerateParams, resp *GenerateResult) (err error) {
+	resp, err = g.Impl.Generate(args)
+	return
+}
+
+// It is a worker for net/rpc.
+type NetRPCWorker struct {
+	Impl Generator
+}
+
+// Returns a server for net/rpc.
+func (n *NetRPCWorker) Server(*plugin.MuxBroker) (interface{}, error) {
+	return &Server{Impl: n.Impl}, nil
+}
+
+// Returns a client for net/rpc.
+func (NetRPCWorker) Client(b *plugin.MuxBroker, c *rpc.Client) (interface{}, error) {
+	return &Client{client: c}, nil
+}
